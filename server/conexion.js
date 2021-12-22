139af3c8df2bae15920cd3e695c1b6b9a82d1fd8
causadol <92407111+causadol@users.noreply.github.com>
2021-12-21 19:39:29 -0500

    conexion
    creamos la conexion y modificamos el Json
    "main": "conexion.js",
      "scripts": {
        "start": "nodemon conexion.js"

---
+++ server/conexion.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//const route = require('./routes/route');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+mongoose.connect('mongodb+srv://katastronk:HsOlG2021@katastronkdb.dnye1.mongodb.net/katastronk?retryWrites=true&w=majority').then(() => {
+    console.log('conexion exitosa');
+    app.listen(5000);
+});
+//app.use("/api",route);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
